132c01be558ce52317e57b445fbc9c6d61b7ccbc
Anna Kasimova <akasimova-86@yandex.ru>
2021-10-30 16:58:42 +0500

    New test

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,7 +4,8 @@ public class SQRService {
     public int numberSQR (int low, int high){
         int counter = 0;
         for (int i = 10; i <=99; i++){
-            if (low <= i * i && i * i <= high){
+            int square = i * i;
+            if (low <= square && square <= high){
                 counter++;
             }
         }
