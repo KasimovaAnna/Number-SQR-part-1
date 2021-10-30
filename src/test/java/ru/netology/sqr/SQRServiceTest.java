d12d8c537334761c49182b24d9774a0d90d16eb9
Anna Kasimova <akasimova-86@yandex.ru>
2021-10-30 16:59:25 +0500

    New testing

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,17 +1,29 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
-@Test
- void shouldNumberSQR() {
+@ParameterizedTest
+@CsvSource (value = {"'NumberSQR, 200-300', 200, 300, 3"})
+ void shouldNumberSQR(String testName, int low, int high, int expected) {
     SQRService service = new SQRService();
-
-    int expected = 3;
 
     int actual = service.numberSQR(200,300);
     assertEquals(expected, actual);
     }
+
+    @Test
+void shouldBoundariesOfNumbers(){
+SQRService service =new SQRService();
+int low = 100;
+int high = 9801;
+int expected = 90;
+
+int actual = service.numberSQR(100, 9801);
+assertEquals(expected, actual);}
 }
+
